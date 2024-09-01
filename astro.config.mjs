2cb4c51e86d5c67d5a60c024466bb4725d9274cc
Emerson Warhman <edwarhman@gmail.com>
2024-08-31 22:49:21 -0400

    create internalized links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
+import { i18n } from './src/i18n/config'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server'
+  output: 'server',
+  i18n
 })
