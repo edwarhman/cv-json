35ff577ec9ddcbd366d88e7fade2153098964b60
Emerson Warhman <edwarhman@gmail.com>
2024-09-01 17:31:11 -0400

    add react and reack-hook-form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config'
-import { i18n } from './src/i18n/config'
+import { i18n } from './src/core/i18n/config'
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  i18n
+  i18n,
+  integrations: [react()]
 })
