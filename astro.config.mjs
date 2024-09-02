78ab6297790d3f5914f367b36b3ae996a3fbba71
Emerson Warhman <edwarhman@gmail.com>
2024-09-02 00:20:19 -0400

    fix astro config for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { i18n } from './src/core/i18n/config'
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
