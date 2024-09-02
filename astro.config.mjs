065aec2dd4d0333ca513bf5b8cd174ed4bfc50d2
Emerson Warhman <edwarhman@gmail.com>
2024-09-02 00:02:11 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { i18n } from './src/core/i18n/config'
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   i18n,
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 })
