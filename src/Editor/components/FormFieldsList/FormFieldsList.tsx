e8a3cedf43d9ffc9c9cc9a42337e42c151bcec88
Emerson Warhman <edwarhman@gmail.com>
2024-09-14 10:35:54 -0400

    change spacing in FieldsList footer

--- src/Editor/components/FormFieldsList/FormFieldsList.tsx
+++ src/Editor/components/FormFieldsList/FormFieldsList.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react"
-import { useFieldArray, type Control, type UseFormRegister } from "react-hook-form"
+import { useFieldArray, type Control } from "react-hook-form"
 import styles from './formFieldsList.module.css'
 import Trash from "@/core/icons/Trash"
 
@@ -17,9 +17,6 @@ export default function ({ control, name, defaultValue, render, onChange, type }
         control,
         name
     })
-    const footerStyle = type === 'internal' ? styles.internalFooter : styles.externalFooter
-    const childrenWrapperInternal = type === 'internal' ? styles.internalChildrenWrapper : ''
-    const footerSpacingType = type === 'internal' ? styles.footerSpacingInternal : ''
     const typeStyle = type === 'internal' ? styles.internal : styles.external
 
     const handleAppend = () => {
@@ -37,20 +34,24 @@ export default function ({ control, name, defaultValue, render, onChange, type }
                 fields.map((field, index) => {
                     const children = render({ index })
                     return (
-                        <div key={field.id} className={`${styles.childrenWrapper} ${childrenWrapperInternal}`}>
+                        <div key={field.id} className={`${styles.fieldWrapper}`}>
                             <button type="button" onClick={() => handleRemove(index)}><Trash /></button>
-                            <div className={styles.children}>
+                            <div className={styles.field}>
                                 {children}
                             </div>
                         </div>
                     )
                 })
             }
-            <div className={`${styles.childrenWrapper} ${styles.footer} ${footerStyle}`}>
-                <button type="button" className={styles.addButtonWrapper} onClick={handleAppend}>
-                    <button className={styles.addButton} type="button">+</button>
-                </button>
-                <div className={styles.footerSpacing}><button className={type === 'internal' ? styles.internalButton : styles.externalButton} disabled>a</button></div>
+            <div className={`${styles.fieldWrapper} ${styles.footer}`}>
+                <div className={styles.footerSpacing}>
+                    <button disabled>a</button>
+                </div>
+                <div className={styles.field}>
+                    <button type="button" className={styles.addButtonWrapper} onClick={handleAppend}>
+                        <button className={styles.addButton} type="button">+</button>
+                    </button>
+                </div>
             </div>
         </div>
     )
