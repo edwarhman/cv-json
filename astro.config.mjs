d3cc5f014d90477a2fb9dd86c30a291223bb166b
Emerson Warhman <edwarhman@gmail.com>
2024-09-18 15:52:52 -0400

    format astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
 import { i18n } from './src/core/i18n/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
