07cd24c0a37155bdb6fcc4a2dd7e00d862cbd85f
Emerson Warhman <edwarhman@gmail.com>
2024-09-21 12:34:55 -0400

    refactor/abstract buttonWrapper to own component BigButton

---
+++ src/Editor/components/BigButton/BigButton.tsx
@@ -0,0 +1,23 @@
+import styles from './bigButton.module.css'
+
+interface Props {
+    children: React.ReactNode
+    type?: 'internal' | 'external'
+    onClick: () => void
+}
+
+export default function BigButton({
+    children,
+    type = 'internal',
+    onClick
+}: Props) {
+
+    const typeStyle = type === 'internal' ? styles.internal : styles.external
+
+    return (
+
+        <button type='button' className={`${styles.bigButton} ${typeStyle}`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
--- src/Editor/components/FormFieldsList/FormFieldsList.tsx
+++ src/Editor/components/FormFieldsList/FormFieldsList.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 import { useFieldArray, type Control } from 'react-hook-form'
 import styles from './formFieldsList.module.css'
 import Trash from '@/core/icons/Trash'
+import BigButton from '../BigButton/BigButton'
 
 interface Props {
   control: Control<any>
@@ -12,7 +13,7 @@ interface Props {
   type?: 'external' | 'internal'
 }
 
-export default function ({ control, name, defaultValue, render, onChange, type }: Props) {
+export default function ({ control, name, defaultValue, render, onChange, type = 'external' }: Props) {
   const { fields, append, remove } = useFieldArray({
     control,
     name
@@ -48,9 +49,9 @@ export default function ({ control, name, defaultValue, render, onChange, type }
           <button className='button'>a</button>
         </div>
         <div className={styles.field}>
-          <button type='button' className={styles.addButtonWrapper} onClick={handleAppend}>
+          <BigButton type={type} onClick={handleAppend}>
             <div className='button'>+</div>
-          </button>
+          </BigButton>
         </div>
       </div>
     </div>
